8b9eb96ffc3930e35ed7fc427312efd0834d3b9f
kristastarr <kstarr0092@gmail.com>
2017-05-12 22:49:50 -0500

    add code to get user input and assign to variables

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "What year were you born?"
+birth_year = gets.to_i
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you? (enter yes or no)"
+bread = gets.chomp
+	
+
+puts "Would you like to enroll in the company's health insurance plan? (enter yes or no)"
+insurance = gets.chomp
+	
