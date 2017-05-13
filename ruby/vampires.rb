5747db3b7b58dce29a80cf3dd151cfd93af61484
kristastarr <kstarr0092@gmail.com>
2017-05-13 10:24:15 -0500

    add work on process multiple employees feature and allergy detection feature

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,57 +7,74 @@ x = 0
 
 while x < number 
 
-	puts "What is your name?"
-	name = gets.chomp
+  	puts "What is your name?"
+  	name = gets.chomp
+  
+  	puts "How old are you?"
+  	age = gets.to_i
+  
+  	puts "What year were you born?"
+  	birth_year = gets.to_i
+  	
+  	puts "Our company cafeteria serves garlic bread.  Should we order some for you? (enter yes or no)"
+  	bread = gets.chomp
+  
+  	puts "Would you like to enroll in the company's health insurance plan? (enter yes or no)"
+  	insurance = gets.chomp	
 
-	puts "How old are you?"
-	age = gets.to_i
+  	loop do 
+      puts "Please enter any allergies and enter done when finished"
+      allergy = gets.chomp
+      
+      if allergy == "sunshine"
+        result = "Probably a vampire."
 
-	puts "What year were you born?"
-	birth_year = gets.to_i
+      else allergy == "done"
+       	break 
+      end
+     end 
+    
+
+  	if age == (2017 - birth_year)
+  		is_lying = false
+  	else
+  		is_lying = true
+  	end
+  
+  	if bread == "yes"
+  		likes_garlic = true
+  	else 
+  		likes_garlic = false
+  	end 
+  
+  	if insurance == "no" 
+  		is_immortal = true
+  	else is_immortal = false
+  	end 
+  	
+  	case 
+  	 when is_lying == false && (likes_garlic == true && is_immortal == false) 
+  		result = "Probably not a vampire."
+  		
+  	when (is_lying == true && likes_garlic == true) || (is_lying == true && is_immortal == false)
+  		result = "Probably a vampire."
+
+  	when name == "Drake Cula" || "Tu Fang"
+      result = "Definitely a vampire."
+
+  	else result =
+  	 "Results inconclusive"	
+
+   end 
+
+    puts "The result is: #{result}"
 	
-	puts "Our company cafeteria serves garlic bread.  Should we order some for you? (enter yes or no)"
-	bread = gets.chomp
+	  x += 1 
+	end 	
 
-	puts "Would you like to enroll in the company's health insurance plan? (enter yes or no)"
-	insurance = gets.chomp	
+	puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
-	if age == (2017 - birth_year)
-		is_lying = false
-	else
-		is_lying = true
-	end
 
-	if bread == "yes"
-		likes_garlic = true
-	else 
-		likes_garlic = false
-	end 
 
-	if insurance == "no" 
-		is_immortal = true
-	else is_immortal = false
-	end 
 
-	if lying == false && (garlic == true && immortal == false) 
-		result = "Probably not a vampire"
-
-	elsif lying == true && (garlic == false && immortal == true)
-		result = "Probably a vampire"
-
-	elsif lying == true && (garlic == false && immortal == true)
-		result = "Almost certainly a vampire"
-
-	elsif name == "Drake Cula" || "Tu Fang"
-		result = "Definitely a vampire"
-
-	else result =
-	 "results inconclusive"	
-	end 
-
-puts "The result is: #{result}"
-
-x += 1
-
-end 
-
+  
