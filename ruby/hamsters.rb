72c57bfb62a41508cea7a5ca37da9e7fb1a22cdf
kristastarr <kstarr0092@gmail.com>
2017-05-14 18:13:37 -0500

    add hamter data input questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+
+puts "What is the hamster's volume level on a scale of 1 - 10?"
+volume_level = gets.to_i 
+
+puts "What color is the hamster's fur?"
+fur_color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? y/n"
+adoption = gets.chomp
+
+puts "What is the hamster's approximate age?"
+age = gets.to_f 
+
