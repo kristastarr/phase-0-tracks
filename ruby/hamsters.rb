5a61ba4fe0fab2e5059652876825698a2b462aad
kristastarr <kstarr0092@gmail.com>
2017-05-15 00:41:01 -0500

    add code to obtain hamster info and print hamster info

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,6 +1,7 @@
 puts "What is the hamster's name?"
 hamster_name = gets.chomp
 
+
 puts "What is the hamster's volume level on a scale of 1 - 10?"
 volume_level = gets.to_i 
 
@@ -13,3 +14,14 @@ adoption = gets.chomp
 puts "What is the hamster's approximate age?"
 age = gets.to_f 
 
+if age == "" 
+	age = nil
+else age = age.to_f 
+
+ end 	
+
+puts "The hamster's name is #{hamster_name}"
+puts "The hamster's volume level is #{volume_level}"
+puts "The hamster's fur color is #{fur_color}"
+puts "Is the hamster a good candidate for adoption? #{adoption}"
+puts "The hamster's approximate age is #{age}"
