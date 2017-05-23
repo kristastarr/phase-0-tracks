1633f3644d609b3d55db59825ee0f1a14877e268
kristastarr <kstarr0092@gmail.com>
2017-05-22 21:54:21 -0500

    add design for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES
+# daschund
+
+#CHARACTERISTICS
+#bark: surprisingly loud
+#personality: stubborn, protective, loyal, snuggly 
+#leg length: 2-4 inches max 
+#body length: much longer than average in relation to leg length 
+#color: varies, often brown or black and brown 
+#name: varies
+
+#BEHAVIOR
+#Refuse to come
+#Refuse to sit
+#Beg for treats
+#Roll over for belly rubs 
+#Burrow into blankets 
