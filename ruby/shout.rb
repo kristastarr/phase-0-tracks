2d9893d2e08dab7135dd0af93a924fef27d64d62
kristastarr <kstarr0092@gmail.com>
2017-05-26 21:36:44 -0500

    Convert standalone Shout module to a Mixin and add driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,40 @@
+
+# Write a Simple Module
+#module Shout
+	#def self.yell_angrily(words)
+		#words + "!!!" + " :("
+	#end 
+
+	#def self.yelling_happily(words)
+		#"OMG " + words + "!!!"
+	#end 	 
+#end 
+
+#p Shout.yell_angrily('Who took the last donut??')
+#p Shout.yelling_happily('kittens')
+
+#Convert a Standalone Module to a Mixin 
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
+	end 	
+
+	def yelling_happily(words)
+		"OMG " + words + "!!!"
+	end 
+end 		
+
+class Cheerleader 
+	include Shout
 	end 
 
-	def self.yelling_happily(words)
-		"OMG " + words + "!!!"
-	end 	 
+class Toddler
+	include Shout
 end 
 
-p Shout.yell_angrily('Who took the last donut??')
-p Shout.yelling_happily('kittens')
+cheerleader = Cheerleader.new
+p cheerleader.yelling_happily("Go Team Go")
+
+toddler = Toddler.new
+p toddler.yell_angrily("Mine")
