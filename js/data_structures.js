f20dfd5fd9d56ab25d01a4292bcd3aa40c356335
kristastarr <kstarr0092@gmail.com>
2017-06-01 21:26:09 -0500

    add data_structures.js file with horses array and colors array

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["pink", "orange", "teal", "avocado green"]
+var horses = ["Thunder", "Angel", "Sport", "Sister"]
+
+colors.push("lavender")
+horses.push("Chloe")
+
+console.log(colors)
+console.log(horses)
---
+++ js/explore.js
@@ -0,0 +1,38 @@
+//Make a function to reverse the letters in a string
+// function takes a string
+// split the string into an array of letters 
+// var string1 = "hello there"
+// string1[0]
+
+// string1.split("") 
+// string1[0]
+
+
+    
+   function reverseString(word){
+     newString = ""
+     for (var i = word.length - 1; i >= 0; i--) { 
+        newString += word[i]; 
+    }
+    
+    return newString
+    }
+    
+    reverseString("Heyy there")
+    
+    // string2 = "he"
+    // string3 = string2 + "y"
+    // console.log(string3)
+    // string4 = string3 + 4
+    
+    var5 = reverseString;
+    
+ reverseString("See you later");
+ 
+    if (1 == 1) { 
+   console.log reverseString("See you later");
+    
+    }
+    
+    
+    
