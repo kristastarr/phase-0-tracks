450b8771b8fccfac02a1388b0daa9d0dac407ef6
kristastarr <kstarr0092@gmail.com>
2017-06-03 11:29:43 -0500

    add Car constructor with an accelerate function, and driver code to test

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,4 @@
+// Create an array
 var colors = ["pink", "orange", "teal", "avocado green"]
 var horses = ["Thunder", "Angel", "Sport", "Sister"]
 
@@ -7,6 +8,7 @@ horses.push("Chloe")
 // console.log(colors)
 // console.log(horses)
 
+// Create an object
 colorful_horses = []
 
 for (var i = 0; i < horses.length; i++) {
@@ -16,4 +18,27 @@ for (var i = 0; i < horses.length; i++) {
 	
 	};
 
-console.log(colorful_horses);
+// console.log(colorful_horses);
+
+// Create a constructor 
+
+function Car(make, model, speed, convertible){
+
+	console.log("My new car:")
+
+	this.make = make;
+	this.model = model;
+	this.current_speed = speed;
+	this.is_convertible = convertible;
+
+	this.accelerate = function(speed) {
+		this.current_speed = speed; 
+		};
+}
+
+car1 =  new Car("Fiat", "500c", 0, true)
+console.log(car1)
+
+car1.accelerate(75)
+
+console.log(car1)
