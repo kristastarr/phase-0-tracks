30dd43a79922b763e43f4faf244846a3d5337323
kristastarr <kstarr0092@gmail.com>
2017-06-03 15:11:04 -0500

    add method to check two objects to see if any of the key value-pairs match

--- js/algos.js
+++ js/algos.js
@@ -24,16 +24,59 @@ function findLongestString(array){
 	console.log(longestString);
 }
 
-var words = ["long phrase", "longer phrase", "longest phrase"];
-var words2 = ["hello", "hi there", "good morning", "hi"];
-var animals = ["dog", "kitten", "horse", "guinea pig", "fish"];
 
-findLongestString(words);
-findLongestString(words2);
-findLongestString(animals);
+// Write a function that takes two objects and checks to see 
+// if the objects share at least one key value pair:
+
+// first compare object keys- 
+// if object keys do not match, return false
+// if object keys match, then check object values 
+// if object values also match, return true
 
 
+var match; 
+function checkMatch(object1, object2){
+var match; 
+	for (var key in object1){
+	  
+		for (var k in object2){
+				if (key == k){
+				   if (object1[key] == object2[k]){
+			        match = true; 
+					 }
+				}
+		}		
+	}	
+	if (match === true){return true;
+	}
+	else {return false}
+}
 
+
+//Driver Code 
+
+// var words = ["long phrase", "longer phrase", "longest phrase"];
+// var words2 = ["hello", "hi there", "good morning", "hi"];
+// var animals = ["dog", "kitten", "horse", "guinea pig", "fish"];
+
+// findLongestString(words);
+// findLongestString(words2);
+// findLongestString(animals);
+
+
+// var nephew1 = {name: "Ivan", age: 5};
+// var nephew2 = {name: "Oscar", age: 1};
+// var niece = {age: 5, name: "Marley"};
+// var nephews = {1: "Ivan", 2: "Oscar"};
+// var foods = {apples: 6, bananas: 5};
+// var data = {name: "Ivan", date: "June 3"};
+
+
+// console.log(checkMatch(nephew1, nephew2));
+// console.log(checkMatch(nephew1, niece));
+// console.log(checkMatch(nephew1, nephews));
+// console.log(checkMatch(nephew1, foods));
+// console.log(checkMatch(data, nephew1));
 
 
 
