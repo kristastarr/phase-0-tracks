bfd08523f8de18feaeb46dc88bb3b2e81619be9d
kristastarr <kstarr0092@gmail.com>
2017-06-12 00:29:02 -0500

    add method to add an entry to the resources table, and add user interface to gather data to enter

--- databases/resources/resource_list.rb
+++ databases/resources/resource_list.rb
@@ -7,7 +7,7 @@ require 'sqlite3'
 db = SQLite3::Database.new("resources.db")
 db.results_as_hash = true
 
-# Method to add a resource to the resource databases
+# Command to add a table of resources to the resource database
 create_resources_table = <<-table1
 	CREATE TABLE IF NOT EXISTS resources(
 		id INTEGER PRIMARY KEY,
@@ -17,81 +17,156 @@ create_resources_table = <<-table1
 		phone VARCHAR(255),
 		url VARCHAR(255),
 		email VARCHAR(255),
-		city_id INT,
+		county_id INT,
 		needs_id INT,
 
-		FOREIGN KEY (city_id) REFERENCES cities(id),
+		FOREIGN KEY (county_id) REFERENCES counties(id),
 		FOREIGN KEY (needs_id) REFERENCES needs(id)
 	)
 table1
 
-create_cities_table = <<-table2
-	CREATE TABLE IF NOT EXISTS cities(
+# Command to add a table of cities to the resource database (establishes foreign keys)
+create_counties_table = <<-table2
+	CREATE TABLE IF NOT EXISTS counties(
 		id INTEGER PRIMARY KEY,
-		city VARCHAR(255)
+		county VARCHAR(255)
 	)
 table2
 
+# Command to add a table of needs to the resource database (establishes foreign keys)
 create_needs_table = <<-table3
 	CREATE TABLE IF NOT EXISTS needs(
 		id INTEGER PRIMARY KEY,
-		city VARCHAR(255)
+		need VARCHAR(255)
 	)
 table3
 
-db.execute(create_cities_table)
+# Using each command to create the tables
+db.execute(create_counties_table)
 db.execute(create_needs_table)
 db.execute(create_resources_table)
 
-
-# def add_resource(db, info)
-# 	db.execute("INSERT INTO resources (name, description, address, phone, website, email, city_id, needs_id) 
-#    VALUES (?,?,?,?,?,?,?,?), ")
-
-# end 
+# Used the terminal to populate the counties and needs tables- 
+# Since the users won't need to make changs to those tables, they don't need a UI
 
 
-# def get_info 
-  
-# 	info = [ ]
-# 	puts "Please enter the name of the organization"
-# 	name = gets.chomp
-# 	info << name 
-
-# 	puts "Please enter a short description of assistance provided"
-# 	description = gets.chomp
-# 	info << description 
-
-# 	puts "Please enter the address"
-# 	address = gets.chomp
-# 	info << address
-
-# 	puts "Please enter the phone number"
-# 	phone = gets.chomp
-# 	info << phone 
-
-# 	puts "Please enter the organization's website address or type none"
-# 	website = gets.chomp
-# 	info << website
-
-# 	puts "Please enter the organization's email address"
-# 	email = gets.chomp 
-# 	info << email
-
-# 	puts "Please enter the city where the organization provides services (INT)"
-# 	city_id = gets.chomp
-# 	info << city_id
-
-# 	puts "Please enter the type of assistance provided (INT)"
-# 	needs_id = gets.chomp 
-# 	info << needs_id 
-
-# 	return info 
-
-# end 
-
-# add resource (db, get info)
+	 
 
 
 
 
+# method that adds a resource to the resources table: 
+def add_resource(db, name, description, address, phone, url, email, county_id, needs_id)
+
+	add_resource_cmd = <<-cmd2
+		INSERT INTO resources 
+		(name, description, address, phone, url, email, county_id, needs_id)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?)  	
+	cmd2
+
+	db.execute(add_resource_cmd, [name, description, address, phone, url, email, county_id, needs_id])
+end 
+
+def resource_search(db, county, need)
+search = db.execute("SELECT * FROM resources")
+results = []
+
+	search.each do |listing|
+		if listing[7] == county_id.to_i && listing[8] == needs_id.to_i
+
+		results << listing
+
+		end 
+	end
+
+p results 	 
+end 	
+
+
+
+# Driver code to test the method:
+# add_resource(db, 
+# 	"Capitol Area Food Bank",
+# 	"Provides food to people in need",
+# 	"123 Main St Austin TX 78702",
+# 	"(512)533-3333", 
+# 	"www.capitolareafoodbank.com",
+# 	"admin@cafb.com",
+# 	1, 
+# 	2)
+
+
+## Driver code - user interface: 
+puts "Welcome to the Community Resource Database!!"
+puts "What do you want to do?"
+puts "Type 1 to add a resource to the database"
+puts "Type 2 to search for a resource in your area"
+puts "Type 3 to exit the program"
+
+input = gets.chomp
+
+if input.to_i == 1
+
+	puts "Please enter the name of the organization"
+	name = gets.chomp
+	
+
+	puts "Please enter a short description of the assistance provided by the organization"
+	description = gets.chomp
+	 
+
+	puts "Please enter the address"
+	address = gets.chomp
+	
+
+	puts "Please enter the phone number"
+	phone = gets.chomp
+	
+
+	puts "Please enter the website address, or type none"
+	url = gets.chomp
+	 
+
+	puts "Please enter the best email or type none"
+	email = gets.chomp
+
+
+	puts "What county does this organization serve?"
+	puts "Enter 1 for Travis County"
+	puts "Enter 2 for Williamson County"
+	puts "Enter 3 for Hayes County"
+	county_id = gets.chomp   
+
+
+	puts "What type of assistance does this organization provide?"
+	puts "Enter 1 for housing"
+	puts "Enter 2 for food"
+	puts "Enter 3 for medical care"
+	puts "Enter 4 for counseling"
+	puts "Enter 5 for children's services"
+	needs_id = gets.chomp
+
+	add_resource(db, name, description, address, phone, url, email, county_id, needs_id)
+
+
+elsif input.to_i == 2
+
+	puts "What county do you live in?"
+	search_county = gets.chomp
+
+	puts "What type of assistance do you need?"
+	search_need = gets.chomp 
+
+	puts search_county
+	puts search_need
+
+	resource_search(db, search_county, search_need)
+
+
+# elsif input.to_i == 3 
+# 	break
+
+else 
+puts "Invalid entry- please try again"	
+
+end 	
