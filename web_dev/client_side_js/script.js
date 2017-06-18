34f9a8b34c35b6cc51c720cb8c0ae092e0bff445
kristastarr <kstarr0092@gmail.com>
2017-06-18 09:43:50 -0500

    add simple html page,use Javascript to manipulate the DOM, and add an event listener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,34 @@
-console.log("The script is running!");
+// Use Javascript to manipulate the DOM 
+var headings = document.getElementsByTagName("h1");
+heading1 = headings[0]
+heading1.style.border = "5px dotted grey";
 
-function addPinkBorder(event) {
-  console.log("click happened! here's the click event:");
-  console.log(event);
-  event.target.style.border = "2px solid pink";
+// Add an event listener 
+
+// function addPinkBorder(event) {
+//   console.log("click happened! here's the click event:");
+//   console.log(event);
+//   event.target.style.border = "2px solid pink";
+// }
+
+// var photo = document.getElementById("pets-photo");
+// photo.addEventListener("click", addPinkBorder);
+
+
+function changeBackgroundPink(){
+	event.target.style.backgroundColor = "DeepPink";
+};
+
+function changeBackgroundTeal(){
+	event.target.style.backgroundColor = "DarkTurquoise";
 }
 
-var photo = document.getElementById("lizard-photo");
-photo.addEventListener("click", addPinkBorder);
+var strong = document.getElementsByTagName("em");
+
+strongWord = strong[0]
+strongWord.addEventListener("click", changeBackgroundPink);
+
+
+strongWord2 = strong[1]
+strongWord2.addEventListener("mouseover", changeBackgroundTeal)
+
